d53fb1e6aebe9fdff1e79be36a6dffe0aa0590fa
cykul <cykul@CYKULTECH>
2017-05-31 17:32:45 +0530

    XML Generator project created by Amol @ 31st May 2017 17:32

---
+++ XMLGenerator/src/com/cykul/rides/xmlgenerator/XMLGenerator.java
@@ -0,0 +1,58 @@
+package com.cykul.rides.xmlgenerator;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class XMLGenerator {
+
+	public static void main(String[] args) throws ParserConfigurationException, TransformerException {
+		
+		DocumentBuilderFactory domBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder domBuilder = domBuilderFactory.newDocumentBuilder();
+		Document dom = domBuilder.newDocument();
+		
+		Element rootElement = dom.createElement("personalInfo");
+		
+		Element firstName = dom.createElement("firstName");
+		firstName.setTextContent("Amol");
+		rootElement.appendChild(firstName);
+		
+		Element lastName = dom.createElement("lastName");
+		lastName.setTextContent("Abhale");
+		rootElement.appendChild(lastName);
+		
+		Element emailID = dom.createElement("emailID");
+		emailID.setTextContent("amol@cykul.com");
+		rootElement.appendChild(emailID);
+		
+		Element mobileNumber = dom.createElement("mobileNumber");
+		mobileNumber.setTextContent("8143495180");
+		rootElement.appendChild(mobileNumber);
+		
+		Element organization = dom.createElement("organization");
+		organization.setTextContent("Learnasium");
+		rootElement.appendChild(organization);
+		
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		
+		Transformer transformer = transformerFactory.newTransformer();
+		
+		DOMSource domSource = new DOMSource(dom);
+		
+		StreamResult streamResult = new StreamResult("/home/cykul/JAVA Projects/sample.xml");
+		
+		transformer.transform(domSource, streamResult);
+		
+		
+	}
+}
